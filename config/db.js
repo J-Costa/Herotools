d727429e9cbc704448eb5ca304aa89d27a54e763
Josue Costa <juncosta@gmail.com>
2020-11-16 16:13:52 -0300

    security update

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 if (process.env.NODE_ENV == "production"){
     module.exports = {mongoURI: process.env.MONGO_URI}
-    console.log(process.env.TESTE)
-    console.log("Produção")
 } else {
     module.exports = {mongoURI: "mongodb://localhost/herotools"}
-    console.log("local")
 }
